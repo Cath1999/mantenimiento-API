1cbdccccee6bfbbafb91bca3648ced4080e697af
Cath1999 <catherine.mena11751@est.uca.edu.ni>
2023-05-15 23:12:14 -0600

    first commit

---
+++ Proyecto-Prolis-paciente/businesdomain/catDepartamento/src/main/java/com/example/catdepartamento/CatDepartamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.catdepartamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatDepartamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatDepartamentoApplication.class, args);
+    }
+
+}
---
+++ Proyecto-Prolis-paciente/businesdomain/catMunicipio/src/test/java/com/example/catmunicipio/CatMunicipioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catmunicipio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatMunicipioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
