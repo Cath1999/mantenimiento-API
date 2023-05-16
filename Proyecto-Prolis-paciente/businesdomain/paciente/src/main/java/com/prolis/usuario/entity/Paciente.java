34aa90e210de04e3a126ae8099a2cea27571d748
jessicaaviles8835 <jessica.aviles8835@est.uca.edu.ni>
2023-05-16 14:16:09 -0600

    paciente entity

--- Proyecto-Prolis-paciente/businesdomain/paciente/src/main/java/com/prolis/usuario/entity/Paciente.java
+++ Proyecto-Prolis-paciente/businesdomain/paciente/src/main/java/com/prolis/usuario/entity/Paciente.java
@@ -1,20 +1,51 @@
 package com.prolis.usuario.entity;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Entity
+@Entity 
 @Data
 public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idpaciente;
-    private String cedula;
-    private String nombres;
-    private String apellidos;
-    private String telefono;
-    private String direccion;
+    private Long IdPaciente;
+    private Long IdIdentificacion;
+    private String NumIdentificacion;
+    private Long NumExpediente;
+    private String NumINSS;
+    private Long IdEstadoCivil;
+    private String Email;
+    private Long IdSexo;
+    private Long IdNacionalidad;
+    private Long IdPaisNac;
+    private Long IdDepartamentoNac;
+    private Long IdMunicipioNac;
+    private Long IdPaisRes;
+    private Long IdDepartamentoRes;
+    private Long IdMunicipioRes;
+    private Long IdTipoSangre;
+    private Long IdProfesiones;
+    private Long idReligion;
+    private String PrimerNombre;
+    private String SegundoNombre;
+    private String PrimerApellido;
+    private String SegundoApellido;
+    private Date FechaNac;
+    private String DireccionDomiciiar;
+    private String TelefonoDomiciliar;
+    private String TelefonoMovil;
+    private String Activo;
+    @Column (name="Emabrazada")
+    private String Embarazada;
+    private String Fallecido;
+    private Short Estado;
+
+    
+    
 }
